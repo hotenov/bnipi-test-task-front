45d012d318e47badd3658daa9c950468dfb40419
Artem Hotenov <qa@hotenov.com>
2024-09-29 20:45:57 +0300

    💚 chore(cd): Switch to manual deploy to GitHub Pages
    Remove workflow 'deploy-vue-app.yml'

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/bnipi-test-task-front/'
+  base: process.env.NODE_ENV === 'production'
+    ? '/bnipi-test-task-front/'
+    : '/'
 })
